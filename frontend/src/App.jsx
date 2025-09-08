df6258113eb2370bffca7602d745d541c31d6a38
alfred-delacosta <132242610+alfred-delacosta@users.noreply.github.com>
2025-09-08 10:05:34 -0400

    Updated some CSS, also updated the meta tags.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,30 +4,25 @@ import ViewImage from "./pages/ViewImage";
 import Grid from "@mui/material/Grid";
 import CssBaseline from "@mui/material/CssBaseline";
 import NotFound from "./pages/NotFound";
+import './css/App.css'
 
 function App() {
   return (
-    <>
-      <CssBaseline />
-      <Grid
-        container
-        justifyContent={"space-around"}
-        alignItems={"center"}
-        bgcolor={`background: #0d65b8;
+    <Grid
+      bgcolor={`background: #0d65b8;
 background: linear-gradient(30deg, rgba(13, 101, 184, 1) 11%, rgba(17, 131, 237, 1) 100%);`}
-        height={"100vh"}
-      >
+      height={"100%"}
+    >
+      <CssBaseline />
+      <Grid container justifyContent={"space-around"} alignItems={"center"}>
         <Routes>
           <Route path="/" element={<FileUpload />} />
-          <Route
-            path="/viewImage/:fileName"
-            element={<ViewImage />}
-          />
+          <Route path="/viewImage/:fileName" element={<ViewImage />} />
           <Route path="/notfound" element={<NotFound />} />
           <Route path="/*" element={<NotFound />} />
         </Routes>
       </Grid>
-    </>
+    </Grid>
   );
 }
 
